f385533741b63ea828cc2190ed3a27747e0763da
BarclayII <coin2028@hotmail.com>
2014-12-02 11:36:39 +0800

    implemented srvr_set_outexec and added some debugging information

--- server/srvr_proto.c
+++ server/srvr_proto.c
@@ -19,6 +19,47 @@ char *srvr_mkdir(const char *user)
 		pinfo(PINFO_WARN, TRUE, "mkdir for %s", user);
 		return NULL;
 	}
+	pinfo(PINFO_DEBUG, "mkdir %s", dir);
 
 	return dir;
+}
+
+/*
+ * Create a ".outexec" configuration file under the specified directory for
+ * output executable specification
+ */
+int srvr_set_outexec(const char *dir, const char *outexec)
+{
+	if (chdir(dir) == -1) {
+		pinfo(PINFO_WARN, TRUE, "chdir to %s", dir);
+		return -1;
+	}
+	pinfo(PINFO_DEBUG, FALSE, "chdir to %s", dir);
+
+	FILE *fp;
+	if ((fp = fopen(OUTEXEC_CONFIG, "w")) == NULL) {
+		pinfo(PINFO_WARN, TRUE, "fopen %s", OUTEXEC_CONFIG);
+		return -1;
+	}
+	pinfo(PINFO_DEBUG, FALSE, "fopen %s", OUTEXEC_CONFIG);
+
+	if (fprintf(fp, "%s\n", outexec) < 0) {
+		pinfo(PINFO_WARN, TRUE, "fprintf %s", OUTEXEC_CONFIG);
+		return -1;
+	}
+	pinfo(PINFO_DEBUG, FALSE, "fprintf %s %s", OUTEXEC_CONFIG, outexec);
+
+	if (fclose(fp) == EOF) {
+		pinfo(PINFO_WARN, TRUE, "fclose %s", OUTEXEC_CONFIG);
+		return -1;
+	}
+	pinfo(PINFO_DEBUG, FALSE, "fclose %s", OUTEXEC_CONFIG);
+
+	if (chdir("..") == -1) {
+		pinfo(PINFO_WARN, TRUE, "return to parent directory failed");
+		return -1;
+	}
+	pinfo(PINFO_DEBUG, FALSE, "return to parent directory");
+
+	return 0;
 }
